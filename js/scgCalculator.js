9f32ef041a0921121550e27659ee0caf98de9305
Anshu2505 <wwwanshu8@gmail.com>
2023-04-28 01:34:03 +0530

    updated css

--- js/scgCalculator.js
+++ js/scgCalculator.js
@@ -1,10 +1,10 @@
 function calculate() {
-    var currentDividend = document.getElementById("currentDividend").value;
-    var growthRate = document.getElementById("growthRate").value / 100;
-    var requiredReturn = document.getElementById("requiredReturn").value / 100;
+    let currentDividend = parseInt(document.getElementById("currentDividend").value);
+    let growthRate = parseInt(document.getElementById("growthRate").value) / 100;
+    let requiredReturn = parseInt(document.getElementById("requiredReturn").value) / 100;
 
-    var nextDividend = currentDividend * (1 + growthRate);
-    var stockPrice = nextDividend / (requiredReturn - growthRate);
-
-    document.getElementById("result").innerHTML = "Stock Price: " + stockPrice.toFixed(2);
+    let nextDividend = currentDividend * (1 + growthRate);
+    let stockPrice = nextDividend / (requiredReturn - growthRate);
+    let result=document.getElementById("result");
+    result.innerHTML= "Stock Price: " + stockPrice.toFixed(2);
 }
